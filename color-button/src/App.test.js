21851078c010dda261b39056950ec0446d0effe9
Richard Levey <riclev123@gmail.com>
2022-10-31 12:40:24 +0200

    fix: test color button initial state

--- color-button/src/App.test.js
+++ color-button/src/App.test.js
@@ -1,8 +1,17 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import { render, screen } from "@testing-library/react";
+import App from "./App";
 
-test('renders learn react link', () => {
+test("button has correct initial color", () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn testing library/i);
-  expect(linkElement).toBeInTheDocument();
+
+  const colorButton = screen.getByRole("button", { name: "Change to blue" });
+  expect(colorButton).toHaveStyle({ backgroundColor: "red" });
+});
+
+test("button has correct initial color", () => {
+  render(<App />);
+});
+
+test("button turns blue when clicked", () => {
+  render(<App />);
 });
